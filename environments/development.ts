5d1ef41dff01ff50d89d863b71c90cd69276b2d7
Davi José Cunha Santos <davijosantos.dj@gmail.com>
2026-03-30 08:50:01 -0300

    hotfix

--- environments/development.ts
+++ environments/development.ts
@@ -1,7 +1,7 @@
 const development = {
   provider: 'nest', // 'nest' as 'nest'
   nest: {
-    apiUrl: 'http://192.168.15.8:8080',
+    apiUrl: 'http://192.168.1.28:8080',
     timeout: 10000,
   }
 }
